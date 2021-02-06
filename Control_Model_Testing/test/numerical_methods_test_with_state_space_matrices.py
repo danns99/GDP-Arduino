69eb3957c521ccfed60fb03cb0afeea0f4b1cc81
Andrew Kernan <37912763+TheOfficialDarthVader@users.noreply.github.com>
2021-02-06 17:22:35 +0000

    Update numerical_methods_test_with_state_space_matrices.py
    Enable plotting the simulation of the target aircraft.

--- Control_Model_Testing/test/numerical_methods_test_with_state_space_matrices.py
+++ Control_Model_Testing/test/numerical_methods_test_with_state_space_matrices.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 # --------------------------------------------------------------------------- #
 
 
-
 # Functions
 # --------------------------------------------------------------------------- #
 def solve_xdot_f_euler(A, B, x_store_0, x_store_1, u, dt, steps):
@@ -21,7 +20,7 @@ def solve_xdot_f_euler(A, B, x_store_0, x_store_1, u, dt, steps):
     return(x)
 
 
-def solve_xdot_b_euler(A, B, x_store_0_var, x_store_1_var, u, dt, steps):
+def solve_xdot_b_euler(A, B, x_store_0, x_store_1, u, dt, steps):
     B_times_u = [0, 0]
     inv_A = [[0, 0],[0, 0]]
     x = [0, 0]
@@ -32,8 +31,8 @@ def solve_xdot_b_euler(A, B, x_store_0_var, x_store_1_var, u, dt, steps):
     inv_A[1][0] = inv_det_A * (A[1][0]*dt)
     inv_A[1][1] = inv_det_A * (1-(A[0][0]*dt))
 
-    B_times_u[0] = x_store_0_var[steps] + (B[0]*0 + B[1]*u[0])*dt
-    B_times_u[1] = x_store_1_var[steps] + (B[0]*0 + B[1]*u[0])*dt
+    B_times_u[0] = x_store_0[steps] + (B[0]*0 + B[1]*u[0])*dt
+    B_times_u[1] = x_store_1[steps] + (B[0]*0 + B[1]*u[0])*dt
 
     x[0] = inv_A[0][0]*B_times_u[0] + inv_A[0][1]*B_times_u[1]
     x[1] = inv_A[1][0]*B_times_u[0] + inv_A[1][1]*B_times_u[1]
@@ -42,16 +41,20 @@ def solve_xdot_b_euler(A, B, x_store_0_var, x_store_1_var, u, dt, steps):
 
 
 def run_sim():
-    A = [[-3.36272133719075, 53.0090457250976],[-1.22585753557309, -5.71920679668875]]
-    B = [-13.4847622573906, -38.384583022159]
+    A_sc = [[-3.36272133719075, 53.0090457250976],[-1.22585753557309, -5.71920679668875]]
+    B_sc = [-13.4847622573906, -38.384583022159]
+    A_t = [[-0.295319442940517, 177.255749314106],[-0.0104482212700093, -0.448358567032906]]
+    B_t = [-6.2938910678574, -4.88848338612147]
     u = [0, 0]
     x = [0, 0]
 
-    x_store_0 = [0]
-    x_store_1 = [0]
+#    x_store_0_euler = [0]
+#    x_store_1_euler = [0]
 
-    x_store_0_var = [0]
-    x_store_1_var = [0]
+    x_store_0_sc_b_euler = [0]
+    x_store_1_sc_b_euler = [0]
+    x_store_0_t_b_euler = [0]
+    x_store_1_t_b_euler = [0]
 
     times = []
 
@@ -62,22 +65,29 @@ def run_sim():
     while(steps<sim_time/dt):
         if steps == 0:
             u[0] = 15 * 3.1415/180
+        
+        # Solve state-space equations for scout
+        x = solve_xdot_b_euler(A_sc, B_sc, x_store_0_sc_b_euler, x_store_1_sc_b_euler, u, dt, steps)
+        x_store_0_sc_b_euler.append(x[0])
+        x_store_1_sc_b_euler.append(x[1])
+        # Solve state-space equations for target
+        x = solve_xdot_b_euler(A_t, B_t, x_store_0_t_b_euler, x_store_1_t_b_euler, u, dt, steps)
+        x_store_0_t_b_euler.append(x[0])
+        x_store_1_t_b_euler.append(x[1])
 
-        x = solve_xdot_b_euler(A, B, x_store_0_var, x_store_1_var, u, dt, steps)
-        x_store_0_var.append(x[0])
-        x_store_1_var.append(x[1])
-
-        x = solve_xdot_f_euler(A, B, x_store_0, x_store_1, u, dt, steps)
-        x_store_0.append(x[0])
-        x_store_1.append(x[1])
+#        x = solve_xdot_f_euler(A, B, x_store_0_euler, x_store_1_euler, u, dt, steps)
+#        x_store_0_euler.append(x[0])
+#        x_store_1_euler.append(x[1])
 
         times.append(dt*steps)
 
         steps += 1
 
-    plt.plot(times, x_store_1[:-1], label='Forward Euler')
-    plt.plot(times, x_store_1_var[:-1], '--', label='Backwards Euler')
+#    plt.plot(times, x_store_1[:-1], label='Forward Euler')
+    plt.plot(times, x_store_1_sc_b_euler[:-1], 'k', label='Scout Backwards Euler')
+    plt.plot(times, x_store_1_t_b_euler[:-1], 'r', label='Target Backwards Euler')
     plt.legend(loc='upper right')
+    plt.xlim(0, sim_time)
 # --------------------------------------------------------------------------- #
     
 # Run simulation
