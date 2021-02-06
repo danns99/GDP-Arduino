8c58f026ba06953a208b269b3d741a86941b04fb
Andrew Kernan <37912763+TheOfficialDarthVader@users.noreply.github.com>
2021-02-06 17:41:02 +0000

    Update numerical_methods_test_with_state_space_matrices.py
    Enable the response of the modified Scout to be calculated and plotted.

--- Control_Model_Testing/test/numerical_methods_test_with_state_space_matrices.py
+++ Control_Model_Testing/test/numerical_methods_test_with_state_space_matrices.py
@@ -40,6 +40,20 @@ def solve_xdot_b_euler(A, B, x_store_0, x_store_1, u, dt, steps):
     return(x)
 
 
+def get_modified_scout_input(q_from_target, error, integral, iteration_time):
+    u_from_error_sum = q_from_target - error
+
+    N = 100.0
+    K_p = -86.717674561182620
+    K_d = 0.637114873357555
+
+    proportional_term = K_p*u_from_error_sum
+    derivative_term = ((K_d * u_from_error_sum) - integral) * N
+
+    integral += iteration_time * derivative_term
+    return([proportional_term + derivative_term], integral)
+
+
 def run_sim():
     A_sc = [[-3.36272133719075, 53.0090457250976],[-1.22585753557309, -5.71920679668875]]
     B_sc = [-13.4847622573906, -38.384583022159]
@@ -55,18 +69,22 @@ def run_sim():
     x_store_1_sc_b_euler = [0]
     x_store_0_t_b_euler = [0]
     x_store_1_t_b_euler = [0]
+    x_store_0_sc_mod_b_euler = [0]
+    x_store_1_sc_mod_b_euler = [0]
 
     times = []
 
-    dt = 0.01
+    integral = 0
+
+    dt = 0.001
     sim_time = 15
     steps = 0
 
     while(steps<sim_time/dt):
         if steps == 0:
             u[0] = 15 * 3.1415/180
-        
-        # Solve state-space equations for scout
+
+        # Solve state-space equations for Scout
         x = solve_xdot_b_euler(A_sc, B_sc, x_store_0_sc_b_euler, x_store_1_sc_b_euler, u, dt, steps)
         x_store_0_sc_b_euler.append(x[0])
         x_store_1_sc_b_euler.append(x[1])
@@ -74,6 +92,14 @@ def run_sim():
         x = solve_xdot_b_euler(A_t, B_t, x_store_0_t_b_euler, x_store_1_t_b_euler, u, dt, steps)
         x_store_0_t_b_euler.append(x[0])
         x_store_1_t_b_euler.append(x[1])
+
+        # Get input into modified Scout
+        u_modified_scout, integral = get_modified_scout_input(x_store_1_t_b_euler[steps], x_store_1_sc_mod_b_euler[steps], integral, dt)
+
+        # Solve state-space equations for modified Scout
+        x = solve_xdot_b_euler(A_t, B_t, x_store_0_sc_mod_b_euler, x_store_1_sc_mod_b_euler, u_modified_scout, dt, steps)
+        x_store_0_sc_mod_b_euler.append(x[0])
+        x_store_1_sc_mod_b_euler.append(x[1])
 
 #        x = solve_xdot_f_euler(A, B, x_store_0_euler, x_store_1_euler, u, dt, steps)
 #        x_store_0_euler.append(x[0])
@@ -86,11 +112,13 @@ def run_sim():
 #    plt.plot(times, x_store_1[:-1], label='Forward Euler')
     plt.plot(times, x_store_1_sc_b_euler[:-1], 'k', label='Scout Backwards Euler')
     plt.plot(times, x_store_1_t_b_euler[:-1], 'r', label='Target Backwards Euler')
+    plt.plot(times, x_store_1_sc_mod_b_euler[:-1], 'b--', label='Modified Scout Backwards Euler')
     plt.legend(loc='upper right')
-    plt.xlim(0, sim_time)
+    plt.xlim(0, 12)
+    plt.ylim(-1, 0.2)
 # --------------------------------------------------------------------------- #
-    
+
 # Run simulation
 # --------------------------------------------------------------------------- #
-run_sim()    
+run_sim()
 # --------------------------------------------------------------------------- #
