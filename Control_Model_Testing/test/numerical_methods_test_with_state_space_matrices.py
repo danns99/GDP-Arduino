c19be78ecf46872a8d33e8ac3a981db61455e491
Andrew Kernan <37912763+TheOfficialDarthVader@users.noreply.github.com>
2021-02-06 17:13:57 +0000

    Update numerical_methods_test_with_state_space_matrices.py
    Groups solving of state-space equations into functions.

--- Control_Model_Testing/test/numerical_methods_test_with_state_space_matrices.py
+++ Control_Model_Testing/test/numerical_methods_test_with_state_space_matrices.py
@@ -1,8 +1,47 @@
 # -*- coding: utf-8 -*-
-import matplotlib as plt
+# Import required modules
+# --------------------------------------------------------------------------- #
+import matplotlib.pyplot as plt
+# --------------------------------------------------------------------------- #
 
 
-def test_matrix():
+
+# Functions
+# --------------------------------------------------------------------------- #
+def solve_xdot_f_euler(A, B, x_store_0, x_store_1, u, dt, steps):
+    B_times_u = [0, 0]
+    x = [0, 0]
+
+    B_times_u[0] = (B[0]*0 + B[1]*u[0])
+    B_times_u[1] = (B[0]*0 + B[1]*u[0])
+
+    x[0] = x_store_0[steps] + dt*(A[0][0]*x_store_0[steps] + A[0][1]*x_store_1[steps] + B_times_u[0])
+    x[1] = x_store_1[steps] + dt*(A[1][0]*x_store_0[steps] + A[1][1]*x_store_1[steps] + B_times_u[1])
+
+    return(x)
+
+
+def solve_xdot_b_euler(A, B, x_store_0_var, x_store_1_var, u, dt, steps):
+    B_times_u = [0, 0]
+    inv_A = [[0, 0],[0, 0]]
+    x = [0, 0]
+
+    inv_det_A = 1 / (((1-(A[0][0]*dt))*(1-(A[1][1]*dt))) - ((-A[0][1]*dt)*((-A[1][0]*dt))))
+    inv_A[0][0] = inv_det_A * (1-(A[1][1]*dt))
+    inv_A[0][1] = inv_det_A * (A[0][1]*dt)
+    inv_A[1][0] = inv_det_A * (A[1][0]*dt)
+    inv_A[1][1] = inv_det_A * (1-(A[0][0]*dt))
+
+    B_times_u[0] = x_store_0_var[steps] + (B[0]*0 + B[1]*u[0])*dt
+    B_times_u[1] = x_store_1_var[steps] + (B[0]*0 + B[1]*u[0])*dt
+
+    x[0] = inv_A[0][0]*B_times_u[0] + inv_A[0][1]*B_times_u[1]
+    x[1] = inv_A[1][0]*B_times_u[0] + inv_A[1][1]*B_times_u[1]
+
+    return(x)
+
+
+def run_sim():
     A = [[-3.36272133719075, 53.0090457250976],[-1.22585753557309, -5.71920679668875]]
     B = [-13.4847622573906, -38.384583022159]
     u = [0, 0]
@@ -16,8 +55,6 @@ def test_matrix():
 
     times = []
 
-    inv_A = [[0, 0],[0, 0]]
-    B_times_u = [0, 0]
     dt = 0.01
     sim_time = 15
     steps = 0
@@ -26,27 +63,11 @@ def test_matrix():
         if steps == 0:
             u[0] = 15 * 3.1415/180
 
-        inv_det_A = 1 / (((1-(A[0][0]*dt))*(1-(A[1][1]*dt))) - ((-A[0][1]*dt)*((-A[1][0]*dt))))
-        inv_A[0][0] = inv_det_A * (1-(A[1][1]*dt))
-        inv_A[0][1] = inv_det_A * (A[0][1]*dt)
-        inv_A[1][0] = inv_det_A * (A[1][0]*dt)
-        inv_A[1][1] = inv_det_A * (1-(A[0][0]*dt))
-
-        B_times_u[0] = x_store_0_var[steps] + (B[0]*0 + B[1]*u[0])*dt
-        B_times_u[1] = x_store_1_var[steps] + (B[0]*0 + B[1]*u[0])*dt
-
-        x[0] = inv_A[0][0]*B_times_u[0] + inv_A[0][1]*B_times_u[1]
-        x[1] = inv_A[1][0]*B_times_u[0] + inv_A[1][1]*B_times_u[1]
-
+        x = solve_xdot_b_euler(A, B, x_store_0_var, x_store_1_var, u, dt, steps)
         x_store_0_var.append(x[0])
         x_store_1_var.append(x[1])
 
-        B_times_u[0] = (B[0]*0 + B[1]*u[0])
-        B_times_u[1] = (B[0]*0 + B[1]*u[0])
-
-        x[0] = x_store_0[steps] + dt*(A[0][0]*x_store_0[steps] + A[0][1]*x_store_1[steps] + B_times_u[0])
-        x[1] = x_store_1[steps] + dt*(A[1][0]*x_store_0[steps] + A[1][1]*x_store_1[steps] + B_times_u[1])
-
+        x = solve_xdot_f_euler(A, B, x_store_0, x_store_1, u, dt, steps)
         x_store_0.append(x[0])
         x_store_1.append(x[1])
 
@@ -57,6 +78,9 @@ def test_matrix():
     plt.plot(times, x_store_1[:-1], label='Forward Euler')
     plt.plot(times, x_store_1_var[:-1], '--', label='Backwards Euler')
     plt.legend(loc='upper right')
-
-test_matrix()    
-
+# --------------------------------------------------------------------------- #
+    
+# Run simulation
+# --------------------------------------------------------------------------- #
+run_sim()    
+# --------------------------------------------------------------------------- #
