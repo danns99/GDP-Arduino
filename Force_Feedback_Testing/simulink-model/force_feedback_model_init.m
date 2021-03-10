6ac12354ae2bc3d9a80e1cc197f95dedc577c486
Andrew Kernan <akernan@btinternet.com>
2021-03-10 19:12:11 +0000

    Change DC motor values

--- Force_Feedback_Testing/simulink-model/force_feedback_model_init.m
+++ Force_Feedback_Testing/simulink-model/force_feedback_model_init.m
@@ -6,15 +6,15 @@ clc
 stop_time = 10;  % seconds
 
 %% motor values
-R_armature = 3.9;  % Resistance (Ohm)
-motor_inductance = 12e-6;  % Inductance (H)
-motor_constant = 0.072e-3*60/(2*pi);  % K (V/(rad/s))
-motor_inertia = 0.01;  % J (g*cm^2)
-motor_damping_constant = 3.5077e-6;  % B (N*m/(rad/s))
+R_armature = 3;  % Resistance (Ohm)
+motor_inductance = 0.5;  % Inductance (H)
+motor_constant = 0.01;  % K (V/(rad/s))
+motor_inertia = 0.01;  % J (kg*m^2)
+motor_damping_constant = 0.1;  % B (N*m/(rad/s))
 
 %% motor state-space
 dc_motor_state_space_A = [0 1 0;
-                          0 -motor_damping_constant/motor_inertia motor_constant/motor_inertia; %0.0274
+                          0 -motor_damping_constant/motor_inertia motor_constant/motor_inertia;
                           0 -motor_constant/motor_inductance -R_armature/motor_inductance];
 dc_motor_state_space_B = [0;
                           0;
